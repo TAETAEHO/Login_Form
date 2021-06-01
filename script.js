960e5a32c196b8c29fbe027eaab0db2e0090c98e
TAETAEHO <xogh9204@gmail.com>
2021-06-01 20:49:35 +0900

    login page

---
+++ script.js
@@ -0,0 +1,37 @@
+//아이디, 성공메시지, 실패메시지, 비밀번호, 비밀번호 확인, 비밀번호 확인 메시지를 가지고 온다.
+const InputUsername = document.querySelector('#username');
+const failureMessage = document.querySelector('.failure-message');
+const successMessage = document.querySelector('.success-message');
+const checkValidId = document.querySelector('.id-button');
+const InputPassword = document.querySelector('#password');
+const InputPasswordRetype = document.querySelector('#password-retype');
+const missmatchMessage = document.querySelector('.missmatch-password');
+
+//아이디 길이가 4자리 이상이면 성공메시지, 그 외에는 실패 메시지
+function isMorethan4Length(value) {
+    if(value.length>=4) {
+        alert('사용 가능한 ID입니다.');
+        // successMessage.classList.remove('hide');
+        // failureMessage.classList.add('hide');
+    } else {
+        alert('ID는 4자리 이상으로 만들어주세요.');
+        // successMessage.classList.add('hide');
+        // failureMessage.classList.remove('hide');
+    }
+}
+
+function isMatched(password1, password2) {
+    if(password1 === password2) {
+        missmatchMessage.classList.add('hide');
+    } else {
+        missmatchMessage.classList.remove('hide');
+    }
+}
+
+checkValidId.onclick = () => {
+    isMorethan4Length(InputUsername.value);
+}
+
+InputPasswordRetype.onkeyup = () => {
+    isMatched(InputPassword.value, InputPasswordRetype.value);
+}
