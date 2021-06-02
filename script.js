f42fb83c43a32950378a1c04d4e644fe40024ab3
TAETAEHO <xogh9204@gmail.com>
2021-06-02 19:18:11 +0900

    fix : Add login-validation

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 //아이디, 성공메시지, 실패메시지, 비밀번호, 비밀번호 확인, 비밀번호 확인 메시지를 가지고 온다.
 const inputUsername = document.querySelector('#username');
 const failureMessage = document.querySelector('.failure-message');
+const failrueOtherMessage = document.querySelector('.failure-message2');
 const successMessage = document.querySelector('.success-message');
-const checkValidId = document.querySelector('.id-button');
 const inputPassword = document.querySelector('#password');
 const inputPasswordRetype = document.querySelector('#password-retype');
 const missmatchMessage = document.querySelector('.missmatch-password');
 
+inputUsername.onkeyup = () => {
+    onlyNumberAndEnglish(inputUsername.value);
+    isMorethan4Length(inputUsername.value);
+}
+
+inputPasswordRetype.onkeyup = () => {
+    isMatched(inputPassword.value, inputPasswordRetype.value);
+}
+
 //아이디 길이가 4자리 이상이면 성공메시지, 그 외에는 실패 메시지
 function isMorethan4Length(value) {
     if(value.length>=4) {
-        alert('사용 가능한 ID입니다.');
-        // successMessage.classList.remove('hide');
-        // failureMessage.classList.add('hide');
+        failureMessage.classList.add('hide');
     } else {
-        alert('ID는 4자리 이상으로 만들어주세요.');
-        // successMessage.classList.add('hide');
-        // failureMessage.classList.remove('hide');
+        successMessage.classList.add('hide');
+        failureMessage.classList.remove('hide');
     }
 }
+
+// 아이디가 영어 또는 영어+숫자의 조합으로 되어야한다.
+function onlyNumberAndEnglish(str) {
+    if(/^[A-Za-z][A-Za-z0-9]*$/.test(str)) {
+        successMessage.classList.remove('hide');
+        failrueOtherMessage.classList.add('hide');
+    } else {
+        successMessage.classList.add('hide');
+        failrueOtherMessage.classList.remove('hide');
+    }
+  }
 
 function isMatched(password1, password2) {
     if(password1 === password2) {
@@ -28,10 +45,3 @@ function isMatched(password1, password2) {
     }
 }
 
-checkValidId.onclick = () => {
-    isMorethan4Length(inputUsername.value);
-}
-
-inputPasswordRetype.onkeyup = () => {
-    isMatched(inputPassword.value, inputPasswordRetype.value);
-}
